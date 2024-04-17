cebc84c357deba2db619710029a1b018266ead1f
SidrahAaishah <sidrahaaishah1@gmail.com>
2024-04-17 18:52:04 +0530

    With some more functions like show_borrow_history

--- code.c
+++ code.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #define max 100
 #define max_borrowed_book 5
+#define max_num_book 1000
 struct book
 {
     char title[max];
@@ -26,6 +27,7 @@ struct user
 }*head_user=NULL;
 // struct user *users[max];
 int num_user=0;
+int num_book=0;
 // int top=-1, top_borr_book[max];
 struct date
 {
@@ -43,48 +45,57 @@ struct borrowed_book
 };
 void add_book()
 {
-    struct book *new = (struct book *)malloc(sizeof(struct book));
-    struct book *temp;
-    printf("Title of book: ");
-    scanf("%s",new->title);
-    printf("Author: ");
-    scanf("%s",new->author);
-    printf("ISBN no. : ");
-    scanf("%d",&new->ISBN);
-    printf("Publication year: ");
-    scanf("%d",&new->pub_year);
-    printf("Genre: ");
-    scanf("%s",new->genre);
-    printf("Quantity: ");
-    scanf("%d",&new->quantity);
-    printf("Avalaibilty status: ");
-    scanf("%s",new->availability);
-    if(head_book==NULL)
+    if(num_book==max_num_book)
     {
-        head_book=new;
-        new->next=NULL;
+        printf("Library has reached it's books limit\n");
     }
     else
     {
-        temp=head_book;
-        while(temp->next!=NULL)
+        num_book++;
+        struct book *new = (struct book *)malloc(sizeof(struct book));
+        struct book *temp;
+        printf("Title of book: ");
+        scanf(" %[^\n]s",new->title);
+        printf("Author: ");
+        scanf(" %[^\n]s",new->author);
+        printf("ISBN no. : ");
+        scanf("%d",&new->ISBN);
+        printf("Publication year: ");
+        scanf("%d",&new->pub_year);
+        printf("Genre: ");
+        scanf(" %[^\n]s",new->genre);
+        printf("Quantity: ");
+        scanf("%d",&new->quantity);
+        printf("Avalaibilty status: ");
+        scanf(" %[^\n]s",new->availability);
+        if(head_book==NULL)
         {
-            temp=temp->next;
+            head_book=new;
+            new->next=NULL;
         }
-        temp->next=new;
-        new->next=NULL;
+        else
+        {
+            temp=head_book;
+            while(temp->next!=NULL)
+            {
+                temp=temp->next;
+            }
+            temp->next=new;
+            new->next=NULL;
+        }
     }
+    
 }
 void update_book()
 {
     char title[max];
     printf("Enter book title to update: ");
-    scanf("%s",title);
+    scanf(" %[^\n]s",title);
     struct book *temp=head_book;
     int flag=0;
     while(temp!=NULL)
     {
-        if(temp->title==title)
+        if(strcmp(temp->title,title)==0)
         {
             flag=1;
             break;
@@ -102,11 +113,11 @@ void update_book()
             {
                 case 1:
                     printf("Enter new title of book: ");
-                    scanf("%s",temp->title);
+                    scanf(" %[^\n]s",temp->title);
                     break;
                 case 2:
                     printf("Enter new author: ");
-                    scanf("%s",temp->author);
+                    scanf(" %[^\n]s",temp->author);
                     break;
                 case 3:
                     printf("Enter new ISBN No. : ");
@@ -118,7 +129,7 @@ void update_book()
                     break;  
                 case 5:
                     printf("Enter new genre: ");
-                    scanf("%s",temp->genre);
+                    scanf(" %[^\n]s",temp->genre);
                     break;
                 case 6:
                     printf("Enter new quantity: ");
@@ -126,7 +137,7 @@ void update_book()
                     break;
                 case 7:
                     printf("Enter  availability status: ");
-                    scanf("%s",temp->availability);
+                    scanf(" %[^\n]s",temp->availability);
                     break;
                 case 8:
                     exit(1);
@@ -147,9 +158,9 @@ void add_user()
         num_user++;
         struct user *new_user = (struct user *)malloc(sizeof(struct user));
         printf("Enter name: ");
-        scanf("%s",new_user->name);
+        scanf(" %[^\n]s",new_user->name);
         printf("Enter id: ");
-        scanf("%s",new_user->id);
+        scanf(" %[^\n]s",new_user->id);
         printf("Enter mobile No. : ");
         scanf("%ld",&new_user->mobile);
         printf("1.Platinum(3 Year plan)\n2.Gold(2 Year plan)\n3.Silver(1 Year plan)\n");
@@ -189,8 +200,61 @@ void add_user()
 }
 void update_user()
 {
-
+    char id[max];
+    int flag=0,ch=0;
+    struct user *ptr = head_user;
+    while(flag!=1)
+    {
+        printf("Enter user id: ");
+        scanf(" %[^\n]s",id);
+        while(ptr!=NULL)
+        {
+            if(strcmp(ptr->id,id)==0)
+            {
+                flag=1;
+                break;
+            }
+            ptr=ptr->next;
+        }
+        if(flag==1)
+        {
+            while(ch!=5)
+            {
+                printf("1.Name\n2.ID\n3.Mobile\n4.Membership Status\n5.Exit\n");
+                printf("Choose what to update: ");
+                scanf("%d",&ch);
+                switch(ch)
+                {
+                    case 1:
+                        printf("Enter new name: ");
+                        scanf(" %[^\n]s",ptr->name);
+                        break;
+                    case 2:
+                        printf("Enter new id: ");
+                        scanf(" %[^\n]s",ptr->id);
+                        break;
+                    case 3:
+                        printf("Enter new mobile: ");
+                        scanf("%ld",ptr->mobile);
+                        break;
+                    case 4:
+                        printf("Enter new Membership Status: ");
+                        scanf(" %[^\n]s",ptr->member_status);
+                        break;
+                    case 5:
+                        exit(1);
+                    default:
+                        printf("Choose valid option\n");
+                }
+            }  
+        }
+        else
+        {
+            printf("Please enter valid user id\n");
+        }
+    }   
 }
+    
 void borrowBook()
 {
     char id[max];
@@ -199,11 +263,11 @@ void borrowBook()
     while(flag!=1)
     {
         printf("Enter user id: ");
-        scanf("%s",id);
+        scanf(" %[^\n]s",id);
         
         while(ptr!=NULL)
         {
-            if(ptr->id==id)
+            if(strcmp(ptr->id,id)==0)
             {
                 flag=1;
                 break;
@@ -222,12 +286,12 @@ void borrowBook()
                 struct borrowed_book *new_borr_book = malloc(sizeof(struct borrowed_book));
                 char title[max];
                 printf("Enter title of book: ");
-                scanf("%s",title);
+                scanf(" %[^\n]s",title);
                 struct book *temp=head_book;
                 int f=0;
                 while(temp!=NULL)
                 {
-                    if(temp->title==title)
+                    if(strcmp(temp->title,title)==0)
                     {
                         f=1;
                         temp->quantity--;
@@ -241,6 +305,9 @@ void borrowBook()
                         scanf("%d",new_borr_book->borrowed_date.month);
                         printf("Year: ");
                         scanf("%d",new_borr_book->borrowed_date.year);
+                        new_borr_book->return_date.day=0;
+                        new_borr_book->return_date.month=0;
+                        new_borr_book->return_date.year=0;
                         if(ptr->head_borrowed_book==NULL)
                         {
                             new_borr_book->next = NULL;
@@ -258,8 +325,7 @@ void borrowBook()
                 }
                 if(f==0)
                 printf("Enter valid book title\n");
-            }
-            
+            }           
         }
         else
         {
@@ -275,10 +341,10 @@ void returnBook()
     while(flag!=1)
     {
         printf("Enter user id : ");
-        scanf("%s",id);
+        scanf(" %[^\n]s",id);
         while(ptr!=NULL)
         {
-            if(ptr->id==id)
+            if(strcmp(ptr->id,id)==0)
             {
                 flag=1;
                 break;
@@ -296,18 +362,18 @@ void returnBook()
             {
                 char title[max];
                 printf("Enter title of book: ");
-                scanf("%s",title);
+                scanf(" %[^\n]s",title);
                 struct book *temp=head_book;
                 int f=0;
                 while(temp!=NULL)   
                 {
-                    if(temp->title==title)
+                    if(strcmp(temp->title,title)==0)
                     {
                         f=1;
                         struct borrowed_book *temp_head = ptr->head_borrowed_book;
                         while(temp_head!=NULL)
                         {
-                            if(temp_head->title == title)
+                            if(strcmp(temp_head->title,title)==0)
                             {
                                 printf("Enter date\n");
                                 printf("Day: ");
@@ -325,33 +391,153 @@ void returnBook()
                     }
                     temp=temp->next;
                 }
-                
-
             }
         }
         else
         {
             printf("Enter valid user id\n");
         }
-
+    }
+}
+void show_borrow_history()
+{
+    char id[max];
+    int flag=0;
+    struct user *ptr = head_user;
+    struct borrowed_book *temp;
+    int n=1;
+    while(flag!=1)
+    {
+        printf("Enter user id: ");
+        scanf(" %[^\n]s",id);
+        while(ptr!=NULL)
+        {
+            if(strcmp(ptr->id, id)==0)
+            {
+                flag=1;
+                break;
+            }
+            ptr=ptr->next;
+        }
+        if(flag==1)
+        {
+            temp=ptr->head_borrowed_book;
+            while(temp!=NULL)
+            {
+                printf("Book %d\n",n);
+                printf("Title:  %[^\n]s\n",temp->title);
+                printf("ISBN: %d\n",temp->ISBN);
+                printf("Borrowed date: %d - %d - %d\n",temp->borrowed_date.day, temp->borrowed_date.month, temp->borrowed_date.year);
+                if(temp->return_date.day==0)
+                {
+                    printf("Return date: Book is not returned yet\n");
+                }
+                else
+                {
+                    printf("Return date:  %d - %d - %d\n\n",temp->return_date.day, temp->return_date.month, temp->return_date.year);
+                }
+                n++;
+                temp=temp->next;
+            }
+        }
+        else
+        {
+            printf("Enter valid user id\n");
+        }
     }
 }
 void main()
 {
-    int t=2;
-    while(t--)
+    int ch=0,n,m;
+    while(ch!=3)
     {
-        add_user();
+        printf("Welcome to Central Library\n\n");
+        printf("1.User\n2.Librarian\n3.Exit\n\n");
+        printf("Choose your type: ");
+        scanf("%d",&ch);
+        printf("\n");
+        switch(ch)
+        {
+            case 1:
+                n=0;
+                while(n!=5)
+                {
+                    printf("1.New user registration\n2.Update user info\n3.Borrow book\n4.Return book\n5.Exit\n\n");
+                    printf("Choose option: ");
+                    scanf("%d",&n);
+                    switch(n)
+                    {
+                        case 1:
+                            add_user();
+                            printf("\n");
+                            break;
+                        case 2:
+                            update_user();
+                            printf("\n");
+                            break;
+                        case 3:
+                            borrowBook();
+                            printf("\n");
+                            break;
+                        case 4:
+                            returnBook();
+                            printf("\n");
+                            break;
+                        case 5:
+                            printf("\n");
+                            break;
+                        default:
+                            printf("Choose valid option\n\n");
+                    }
+                }
+                break;
+            case 2:
+                m=0;
+                while(m!=3)
+                {
+                    printf("1.Add new book\n2.Update a book\n3.Exit\n\n");
+                    printf("Choose option: ");
+                    scanf("%d",&m);
+                    switch(m)
+                    {
+                        case 1:
+                            add_book();
+                            printf("\n");
+                            break;
+                        case 2:
+                            update_book();
+                            printf("\n");
+                            break;
+                        case 3:
+                            printf("\n");
+                            break;
+                        default:
+                            printf("Choose valid option\n\n");
+                    }
+                }
+                break;
+            case 3:
+                exit(1);
+                break;
+            default:
+                printf("Choose valid option\n\n"); 
+        }
+        
     }
-    printf("\n");
-    struct user *temp = head_user;
-    while(temp!=NULL)
-    {
-        printf("Name: %s\n",temp->name);
-        printf("Id: %s\n",temp->id);
-        printf("Mobile: %ld\n",temp->mobile);
-        printf("Member status: %s\n",temp->member_status);
-        printf("Number of books borrowed: %d\n",temp->num_borrowed_book);
-        temp=temp->next;
-    }
+    // int t=2;
+    // while(t--)
+    // {
+    //     add_user();
+    // }
+    // printf("\n");
+    // struct user *temp = head_user;
+    // while(temp!=NULL)
+    // {
+    //     printf("Name: %s\n",temp->name);
+    //     printf("Id: %s\n",temp->id);
+    //     printf("Mobile: %ld\n",temp->mobile);
+    //     printf("Member status: %s\n",temp->member_status);
+    //     printf("Number of books borrowed: %d\n",temp->num_borrowed_book);
+    //     temp=temp->next;
+    // }
 }
